f1313d346941181714569b35d04907f1d68ab28e
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-03-30 09:41:12 +0100

    Import jquery ui into app.js, without checking.

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import 'jquery-ui/jquery-ui.index.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
